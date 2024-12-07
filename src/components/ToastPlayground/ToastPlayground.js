7b0e9e8b462bb4953f3b13ef89c0b1b1632ed74b
Vitor Correia <vitor.correia@points.com>
2024-12-07 14:32:28 -0500

    exercise 2

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,19 +1,20 @@
 import React from 'react';
 
 import Button from '../Button';
+import Toast from '../Toast';
 
 import styles from './ToastPlayground.module.css';
 
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
-  const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0])
-  const [message, setMessage] = React.useState('')
+  const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
+  const [message, setMessage] = React.useState('');
+  const [formSubmitted, setFormSubmitted] = React.useState(false);
 
   function handleSubmit(event) {
-    event.preventDefault()
-
-    setMessage('');
+    event.preventDefault();
+    setFormSubmitted(true)
   }
 
   return (
@@ -22,6 +23,11 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+
+      {formSubmitted ? 
+        <Toast message={message} variant={variant} isToastDisplayed={setFormSubmitted}/>
+        : ''  
+      }
 
       <form className={styles.controlsWrapper} onSubmit={handleSubmit}>
         <div className={styles.row}>
